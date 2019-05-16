df23196f15d3b1d8bd9a97fa64cf0c844e1bb3eb
Kuenhwi <kueniyam@naver.com>
2019-05-16 23:35:45 +0900

    UPDATE app.py and ADD todo_dao.py
    dao는 database access object의 줄임말로써 데이터베이스 접근 전용 객체라는 뜻이다. todo_dao.py는 dao로써 분할해 두었다.
    
    이제 백엔드는 완성되었다. 세밀한 부분해서는 아직 부족한 점이 많지만, 기능상으로는 전부 구현되었고 이제 프론트엔드 작업을 시작하면 되겠다.

---
+++ app.py
@@ -0,0 +1,126 @@
+from flask import Flask, jsonify, request, current_app, render_template
+from sqlalchemy import create_engine
+from todo_dao import TodoDao
+
+def create_app(db_config = None):
+    app = Flask(__name__)
+
+    if db_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(db_config)
+
+    database = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
+    app.database = database
+
+    todoDao = TodoDao(database)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/ping', methods=['GET'])
+    def ping():
+        return 'pong'
+
+    @app.route('/todo/all', methods=['GET'])
+    def get_todo_all():
+        """
+        모든 todo에 대한 데이터 전송
+        """
+
+        return jsonify({
+            'todo_all': todoDao.select_todo_all()
+        })
+
+    @app.route('/todo', methods=['POST'])
+    def post_todo_item():
+        """
+        요청 받은 데이터를 todo 항목 추가
+
+        :request.json: 'title', 'content', ('deadline')
+        :return: 추가한 데이터('id', 'title', 'content', 'deadline', 'is_completed', 'priority')
+        """
+        new_item = request.json
+
+        required = ['title', 'content']
+        if not all(key in new_item for key in required):
+            return 'Error: required key - "title", "content", ("deadline")', 400
+
+        if 'deadline' not in new_item:
+            new_item['deadline'] = None
+
+        rowcount, lastrowid = todoDao.insert_todo_item(new_item)
+
+        if rowcount == 1:
+            return jsonify(todoDao.select_todo_item(lastrowid))
+        elif rowcount == 0:
+            return 'Error: Insert fail', 400
+        else:
+            return 'Error: Many tuple inserted', 500
+
+    @app.route('/todo/<int:id>', methods=['PUT'])
+    def put_todo_item(id):
+        """
+        요청 받은 todo 항목을 수정(완료 포함)
+
+        :request.json: 'title', 'content', 'is_completed', ('deadline')
+        """
+        new_item = request.json
+
+        required = ['title', 'content', 'is_completed']
+        if not all(key in new_item for key in required):
+            return 'Error: required key - "title", "content", "is_completed", ("deadline")', 400
+
+        new_item['id'] = id
+
+        if 'deadline' not in new_item:
+            new_item['deadline'] = None
+
+        rowcount = todoDao.update_todo_item(new_item)
+        if rowcount == 1:
+            return jsonify(todoDao.select_todo_item(id))
+        elif rowcount == 0:
+            return 'Error: Update fail', 400
+        else:
+            return 'Error: Many tuple updated', 500
+
+    @app.route('/todo/<int:id>', methods=['DELETE'])
+    def delete_todo_item(id):
+        """
+        요청 받은 todo 항목을 제거
+        """
+
+        rowcount = todoDao.delete_todo_item(id)
+        if rowcount == 1:
+            return '삭제되었습니다.', 200
+        elif rowcount == 0:
+            return 'Error: Delete fail', 400
+        else:
+            return 'Error: Many tuple deleted', 500
+
+    @app.route('/todo/priority', methods=['POST'])
+    def change_todo_priority():
+        """
+        요청 받은 두 todo 항목의 priority를 swap
+
+        request.json = {
+            "item_1": { "id":value, "priority":value },
+            "item_2": { "id":value, "priority":value }
+        }
+
+         - 이 로직은 무결성이 침해될 여지가 있다. 나중에 더 좋은 로직을 알아보아야 겠다.
+        """
+        item_1 = request.json['item_1']
+        item_2 = request.json['item_2']
+
+        rowcount = todoDao.swap_todo_priority(item_1, item_2)
+        if rowcount == 2:
+            return '우선순위가 조정되었습니다.', 200
+        elif rowcount == 0:
+            return 'Error: Swap fail', 400
+        else:
+            return 'Error: only one value modified', 500
+
+    return app
+
---
+++ todo_dao.py
@@ -0,0 +1,116 @@
+from sqlalchemy import text
+
+class TodoDao:
+    def __init__(self, database):
+        self.db = database
+
+    def select_todo_all(self):
+        todo_all = self.db.execute(text("""
+            SELECT
+                id,
+                title,
+                content,
+                deadline,
+                is_completed,
+                priority
+            FROM todo_list
+        """)).fetchall()
+
+        return [{
+                'id': item['id'],
+                'title': item['title'],
+                'content': item['content'],
+                'deadline': item['deadline'],
+                'is_completed': item['is_completed'],
+                'priority': item['priority']
+        } for item in todo_all]
+
+    def select_todo_item(self, item_id):
+        item = self.db.execute(text("""
+        SELECT
+            id,
+            title,
+            content,
+            deadline,
+            is_completed,
+            priority
+        FROM todo_list
+        WHERE id = :id
+        LIMIT 1
+        """), {'id': item_id}).fetchone()
+
+        return {
+            'id': item['id'],
+            'title': item['title'],
+            'content': item['content'],
+            'deadline': item['deadline'],
+            'is_completed': item['is_completed'],
+            'priority': item['priority']
+        } if item else None
+
+    def insert_todo_item(self, new_item):
+        result = self.db.execute(text("""
+            INSERT INTO todo_list(
+                title,
+                content,
+                deadline,
+                priority
+            ) VALUES(
+                :title,
+                :content,
+                :deadline,
+                (
+                SELECT IFNULL(MAX(priority)+1, 0)
+                FROM todo_list t
+                )
+            )
+            LIMIT 1
+        """), new_item)
+
+        return result.rowcount, result.lastrowid
+
+    def update_todo_item(self, new_item):
+        result = self.db.execute(text("""
+            UPDATE todo_list
+            SET
+                title = :title,
+                content = :content,
+                deadline = :deadline,
+                is_completed = :is_completed
+            WHERE
+                id = :id
+            LIMIT 1
+            """), new_item)
+
+        return result.rowcount
+
+    def delete_todo_item(self, item_id):
+        result = self.db.execute(text("""
+            DELETE FROM todo_list
+            WHERE id = :id
+            LIMIT 1
+            """), {'id': item_id})
+
+        return result.rowcount
+
+    def swap_todo_priority(self, item_1, item_2):
+        param = {
+           'id_1': item_1['id'],
+           'id_2': item_2['id'],
+           'priority_1': item_1['priority'],
+           'priority_2': item_2['priority']
+        }
+
+        # THEN 절 뒤에 서브 쿼리를 이용하여 SELECT 하는 방법도 생각해 보았지만 오류가 발생한다.
+        result = self.db.execute(text("""
+            UPDATE todo_list t
+            SET
+                t.priority = CASE
+                    WHEN t.id = :id_1 THEN :priority_2
+                    WHEN t.id = :id_2 THEN :priority_1
+                END
+            WHERE t.id in (:id_1, :id_2)
+            LIMIT 2
+        """), param)
+
+        return result.rowcount
